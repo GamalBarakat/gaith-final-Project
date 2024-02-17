d9db3d15af5f5aafd061edb8c789bfdc5fe9eb18
gamal1511580@gmail.com <gamal1511580@gmail.com>
2024-02-17 18:08:32 +0200

    بسم الله توكلنا على الله

---
+++ lib/Core/Commponent/default_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+//
+// import '../conts/styles.dart';
+// class DefaultButton extends StatelessWidget {
+//   const DefaultButton({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Container(
+//         height: 40.0.h,
+//         decoration: BoxDecoration(
+//           color: Colors.blue,
+//           borderRadius: BorderRadius.all(Radius.circular(8)),
+//         ),
+//         width: double.infinity,
+//         child:
+//         Center(
+//           child:
+//
+//           Text(
+//             'الانتقال للموقع الإلكتروني',
+//             style: Styles.textStyleTitleBody3Regular.copyWith(color: Color(0xffFFFFFF)),
+//           ),
+//         )
+//
+//     );
+//   }
+// }
